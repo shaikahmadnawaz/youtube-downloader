2bbdf282005939390b266a4d1148d8fd411c31e1
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2025-01-15 23:41:46 +0530

    feat(server): added meta data to server

--- server/app/main.py
+++ server/app/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from app.routes import videos
 
-app = FastAPI(title="YouTube Video Downloader")
+version = "v1"
 
-app.include_router(videos.router, prefix="/videos", tags=["Videos"])
+app = FastAPI(title="YouTube Video Downloader",
+description="This is a simple API that allows you to download YouTube videos in different formats.",
+version=version,
+)
 
 
+app.include_router(videos.router, prefix=f"/api/{version}/videos", tags=["Videos"], )
 
-@app.get("/")
+
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
     return {"message": "Welcome to the YouTube Downloader API!"}
